e8cca1ed5eaa4ee9ec18cda8b2e50edf34929f25
PasikD <167731692+PasikD@users.noreply.github.com>
2024-09-23 23:40:22 +0700

    say

---
+++ Say_my_nameTests/Say_my_nameTests.swift
@@ -0,0 +1,17 @@
+//
+//  Say_my_nameTests.swift
+//  Say_my_nameTests
+//
+//  Created by Денис Васильевич on 23.09.2024.
+//
+
+import Testing
+@testable import Say_my_name
+
+struct Say_my_nameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
